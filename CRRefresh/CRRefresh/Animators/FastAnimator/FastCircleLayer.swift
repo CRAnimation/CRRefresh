a7468d1a19c18833175fe937bbc9a76a22220f98
grishutin <grishutin@bifit.com>
2020-01-13 13:30:49 +0300

    Display fix in debug view hierarchy

--- CRRefresh/CRRefresh/Animators/FastAnimator/FastCircleLayer.swift
+++ CRRefresh/CRRefresh/Animators/FastAnimator/FastCircleLayer.swift
@@ -73,6 +73,13 @@ class FastCircleLayer: CALayer {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override init(layer: Any) {
+        color = .init(rgb: (214, 214, 214))
+        pointColor = .init(rgb: (165, 165, 165))
+        lineWidth = 1
+        super.init(layer: layer)
+    }
     
     //MARK: Public Methods
     func startAnimation() {
